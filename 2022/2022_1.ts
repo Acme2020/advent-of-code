676b88a8fb30c068028c94abba1a6a5b8b380a88
Acme2020 <stefandrescher82@googlemail.com>
2025-08-28 20:15:06 +0200

    AoC-22: Solution Part 1.1

--- 2022/2022_1.ts
+++ 2022/2022_1.ts
@@ -1,5 +1,33 @@
 import { getInput } from "../getInput";
 
-const input = getInput("/Users/stefandrescher/Projekte/AoC/2022/input.txt");
+const input =
+  getInput("/Users/stefandrescher/Projekte/AoC/2022/input.txt") ?? [];
 
 console.log(input);
+
+//Task: Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+
+//Step1: sum up values up to empty string
+
+const calories = (): number => {
+  const { maxCalories } = input.reduce(
+    (acc, cal) => {
+      if (cal === "") {
+        return {
+          maxCalories: Math.max(acc.maxCalories, acc.sum),
+          sum: 0,
+        };
+      }
+      return {
+        maxCalories: acc.maxCalories,
+        sum: acc.sum + Number(cal),
+      };
+    },
+    { maxCalories: 0, sum: 0 }
+  );
+  return maxCalories;
+};
+
+//Step2: find maximum of sums
+
+console.log(calories());
